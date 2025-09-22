53c586a7d0e1f0290c3f34da3232003d154e782f
ramkuamarji8756 <168971592+ramkuamarji8756@users.noreply.github.com>
2025-09-22 17:42:03 +0530

    Add files via upload
    Task Gateway

---
+++ Task-Gateway-API/src/main/java/com/task/TaskGatewayApiApplication.java
@@ -0,0 +1,13 @@
+package com.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskGatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskGatewayApiApplication.class, args);
+	}
+
+}
